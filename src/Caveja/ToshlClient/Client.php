7bd0e17917249e19993f283bb9f2f37096d42a3d
Davide Bellettini <davide.bellettini@gmail.com>
2013-12-16 18:07:41 +0100

    Adding $clientId parameter

--- src/Caveja/ToshlClient/Client.php
+++ src/Caveja/ToshlClient/Client.php
@@ -16,11 +16,18 @@ class Client implements ClientInterface
     private $client;
 
     /**
+     * @var string
+     */
+    private $clientId;
+
+    /**
      * Constructor
      * @param GuzzleClientInterface $client
+     * @param string                $clientId
      */
-    public function __construct(GuzzleClientInterface $client)
+    public function __construct(GuzzleClientInterface $client, $clientId)
     {
         $this->client = $client;
+        $this->clientId = $clientId;
     }
 }
